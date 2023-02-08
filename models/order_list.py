89ed3c65d375dd4c41e71e24f5d71eed09b78df3
SShingler <s.shingler@hotmail.co.uk>
2023-02-08 17:04:40 +0000

    4 MVP + order pages + CSS

--- models/order_list.py
+++ models/order_list.py
@@ -2,6 +2,7 @@ from models.order import *
 
 order1 = Order ("John Smith", "8th February 2023", "Lord of the Rings", 1 )
 order2 = Order ("Helen Smith", "8th February 2023", "The Hobbit", 1)
+order3 = Order ("Ronald McDonald", "10th March 2022", "Burgers for beginners", 1000)
 
-orders = [order1, order2]
+orders = [order1, order2, order3]
 
